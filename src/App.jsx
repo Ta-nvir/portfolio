221d11fde5444f2b4a222cba70d0ff7b211d1b48
Tanvir Yaligar <tanviryaligar@gmail.com>
2026-01-28 21:17:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Education from './Components/Education/Education'
 import Project from './Components/Projects/Project'
 import Certification from './Components/Certifications/Certification'
 import Contact from './Components/Contact/Contact'
-import './Global.css'
+import './global.css'
 
 
 const App = () => {
